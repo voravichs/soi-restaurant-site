6d9fda7a3cf47ca153a434010d14424b9984a609
VoravichS <vsilapac@gmail.com>
2023-06-29 19:08:44 -0500

    build tailwind css for easier dev, added routing to pages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import Tabs from '../Tabs/Tabs';
 //import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({ currentPage, handlePageChange }) => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
@@ -16,10 +16,50 @@ const Navbar = () => {
         <div label="Soi 3"></div>
       </Tabs>
       <ul className="col-span-2 xl:col-span-5 hidden md:flex flex-1 md:justify-end xl:justify-center items-center list-none app__navbar-links">
-        <li className="p__opensans mx-8 hover:text-gray-400"><a href="#home">Home</a></li>
-        <li className="p__opensans mx-8 hover:text-gray-400"><a href="#about">About</a></li>
-        <li className="p__opensans mx-8 hover:text-gray-400"><a href="#menu">Menu</a></li>
-        <li className="p__opensans mx-8 hover:text-gray-400"><a href="#contact">Contact</a></li>
+        <li className="p__opensans mx-8 hover:text-gray-400">
+          <a 
+            href="#home"
+            onClick={() => handlePageChange('Home')}
+            className={currentPage === 'Home'
+              ? 'underline font-black drop-shadow-xl'
+              : ''}
+          >
+            Home
+          </a>
+        </li>
+        <li className="p__opensans mx-8 hover:text-gray-400">
+          <a 
+            href="#aboutus"
+            onClick={() => handlePageChange('AboutUs')}
+            className={currentPage === 'AboutUs'
+              ? 'underline font-black drop-shadow-xl'
+              : ''}
+          >
+            About Us
+          </a>
+        </li>
+        <li className="p__opensans mx-8 hover:text-gray-400">
+          <a 
+            href="#menu"
+            onClick={() => handlePageChange('Menu')}
+            className={currentPage === 'Menu'
+              ? 'underline font-black drop-shadow-xl'
+              : ''}
+          >
+            Menu
+          </a>
+        </li>
+        <li className="p__opensans mx-8 hover:text-gray-400">
+          <a 
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            className={currentPage === 'Contact'
+              ? 'underline font-black drop-shadow-xl'
+              : ''}
+          >
+            Contact Us
+          </a>
+        </li>
       </ul>
       <div className="col-span-2 xl:col-span-2 hidden md:flex justify-end items-center">
         <a href="#login" className="p__opensans mr-8 transition-all decoration-2 decoration-yellow-300 hover:underline">Log In / Register</a>
@@ -32,11 +72,45 @@ const Navbar = () => {
           <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
             <MdOutlineRestaurantMenu fontSize={40} className="overlay__close" onClick={() => setToggleMenu(false)} />
             <ul className="app__navbar-smallscreen_links">
-              <li className="p__opensans"><a href="#home">Home</a></li>
-              <li className="p__opensans"><a href="#about">About</a></li>
-              <li className="p__opensans"><a href="#menu">Menu</a></li>
-              <li className="p__opensans"><a href="#awards">Awards</a></li>
-              <li className="p__opensans"><a href="#contact">Contact</a></li>
+              <li className="p__opensans">
+                <a 
+                  href="#home"
+                  onClick={() => {
+                    handlePageChange('Home');
+                    setToggleMenu(false)}}
+                >
+                  Home
+                </a>
+              </li>
+              <li className="p__opensans">
+              <a 
+                href="#aboutus"
+                onClick={() => {
+                  handlePageChange('AboutUs')
+                  setToggleMenu(false)}}
+              >
+                About Us
+              </a>
+              </li>
+              <li className="p__opensans">
+              <a 
+                href="#menu"
+                onClick={() => {
+                  handlePageChange('Menu')
+                  setToggleMenu(false)}}
+              >
+                Menu
+              </a>
+              </li>
+              <li className="p__opensans">
+              <a 
+                href="#contact"
+                onClick={() => {handlePageChange('Contact')
+                setToggleMenu(false)}}
+              >
+                Contact Us
+              </a>
+              </li>
             </ul>
           </div>
         )}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './Home';
+import AboutUs from './AboutUs';
+import Menu from './Menu';
+import Contact from './Contact';
+
+export {
+  Home,
+  AboutUs,
+  Menu,
+  Contact,
+};
